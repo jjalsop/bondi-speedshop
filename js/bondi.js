c76e84b677188be002f152a5aec1cfd81c9b4829
Jackson Alsop <jjalsop@gmail.com>
2015-03-22 14:22:05 +1100

    + mobile nav & mobile fixes

--- js/bondi.js
+++ js/bondi.js
@@ -1,6 +1,17 @@
 $(document).ready(function(){
+
+	// init foundation (for form validation via abide)
 	$(document).foundation();
+
+	// current page indicator
 	var currentPage = $('body').attr('id')
 	console.log(currentPage)
 	$('.' + currentPage).parent().addClass('active-page')
+
+	// mobile menu trigger
+	$('.mobile-menu-trigger a').click(function(e){
+		e.preventDefault()
+		$('#main-nav ul').toggle()
+	})
+
 })
