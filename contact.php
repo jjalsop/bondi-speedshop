1606f254dff12e259e0210736dd9501219a759dd
Jackson Alsop <jjalsop@gmail.com>
2015-03-22 13:46:54 +1100

    + contact page & forms

---
+++ contact.php
@@ -0,0 +1,48 @@
+<?php include("inc/head-one.html"); ?>
+  <title>Bondi Speed Shop – Cars</title>
+<?php include("inc/head-two.html"); ?>
+<body id="contact">
+<?php include("inc/nav.html"); ?>
+  <section id="contact" class="page-hero">
+    <div class="introduction">
+      <h1>Contact</h1>
+    </div>
+  </section>
+  <section class="content-section">
+    <div class="row">
+      <div class="medium-4 columns medium-centered">
+        <h3>Get in touch</h3>
+      </div>
+    </div>
+    <div class="row">
+      <div class="medium-4 columns medium-centered">
+        <form action="#">
+          <div class="row">
+            <div class="medium-12 columns">
+              <input type="text" placeholder="First name">
+            </div>
+          </div>
+          <div class="row">
+            <div class="medium-12 columns">
+              <input type="text" placeholder="Last name">
+            </div>
+          </div>
+          <div class="row">
+            <div class="medium-12 columns">
+              <input type="email" placeholder="Email address">
+            </div>
+          </div>
+          <div class="row">
+            <div class="medium-12 columns">
+              <textarea name="message" id="message" cols="30" rows="10" placeholder="Your message"></textarea>
+            </div>
+          </div>
+          <submit class="button full-width">Send</submit>
+        </form>
+      </div>
+    </div>
+  </section>
+
+  <?php include("inc/footer.html"); ?> 
+</body>
+<?php include("inc/scripts.html"); ?> 
