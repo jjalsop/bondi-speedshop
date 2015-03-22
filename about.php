e936fe62bf55e15f9e7b312497fdf57638ab02c1
Jackson Alsop <jjalsop@gmail.com>
2015-03-22 13:07:43 +1100

    + footer & about page

--- about.php
+++ about.php
@@ -23,5 +23,24 @@
   	</div>
   </section>
   <section class="strip strip-one"></section>
+  <section id="owner-founder" class="content-section">
+  	<div class="row">
+  		<div class="medium-6 columns medium-centered">
+  			<h3>Owner & Founder</h3>
+  		</div>
+  	</div>
+  	<div class="row">
+  		<div class="medium-5 columns medium-offset-1">
+  			<p>he’s always loved cars, but his love of muscle cars started in 1991 when he took possession of a falcon xp coupe and added a 289 V8 to give the thing a little more punch. </p>
+  			<p>after a string of hot ‘60s aussie muscle, he focused on another lifelong passion, ‘bikes’, and in 1994 he built perhaps the first roadworthy harley flat tracker in his dover heights garage. </p>
+  		</div>
+  		<div class="medium-5 columns end">
+  			<p>since then,  he’s bought, owned and sold all manner of muscular rides including his impeccable shelby GT 350 with a fully worked race engine.</p>
+  			<p>these days, skip gets around in a ‘63 falcon xl ute (one of the poster cars for bondi speedshop) with a 289 thrown in for good measure, and tweaked to american ranchero specs. </p>
+  		</div>
+  	</div>
+  </section>
+  <section class="strip strip-two"></section>
+  <?php include("inc/footer.html"); ?> 
 </body>
 <?php include("inc/scripts.html"); ?> 
