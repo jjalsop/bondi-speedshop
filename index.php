e3ede32aec68bd068c3c5823cf444748eb2089a9
Jackson Alsop <jjalsop@gmail.com>
2015-03-22 12:25:53 +1100

    split homepage out into templates

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php include("inc/head-one.html"); ?>
+  <title>Bondi Speed Shop</title>
+<?php include("inc/head-two.html"); ?>
+<body>
+<?php include("inc/nav.html"); ?>
+  <section id="home">
+    <div class="introduction">
+      <h1>Bondi Speedshop</h1>
+      <p>everything in moderation except for cubic inches</p>
+    </div>
+  </section>
+  <section class="strip strip-one home-strip"></section>
+</body>
+<?php include("inc/scripts.html"); ?> 
+
